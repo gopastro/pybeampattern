267a7898b9d31c8be365b076ee673b889b2d54dc
Gopal Narayanan <gopal@astro.umass.edu>
2013-06-05 14:40:50 -0400

    Bug fix

--- beampattern/map/range_map.py
+++ beampattern/map/range_map.py
@@ -123,7 +123,7 @@ class AzimuthMap(object):
         for az in azimuths:
             self.uni.set_azimuth(az, self.azimuth.xmap_vel)
             wait = (abs(az-self.uni.pos_az)/self.azimuth.xmap_vel) + 2.0
-            logger.info("Sleeping for %.2f seconds while stage gets to %.1 degrees" % (wait, az))
+            logger.info("Sleeping for %.2f seconds while stage gets to %.1f degrees" % (wait, az))
             time.sleep(wait)
             fp.write("%.3f\t")
             for freq in self.synth.freq:
