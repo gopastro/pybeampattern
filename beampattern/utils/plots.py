21ea58db92512c0a383ce02a40e0bc8b91bb7788
Gopal Narayanan <gopal@astro.umass.edu>
2013-10-11 13:14:21 -0400

    Add abilityto plot phase

--- beampattern/utils/plots.py
+++ beampattern/utils/plots.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from beampattern.utils.configuration import Configuration
+from beampattern.utils.phase_configuration import ConfigurationPhase
 from beampattern.utils.beampattern_exceptions import BeamPatternGeneralError, BeamPatternArgumentError
 import matplotlib.pyplot as plt
 import re
@@ -134,5 +135,169 @@ class BeamPlot(object):
                         title=title)
 
 
+class BeamPlotPhase(object):
+    def __init__(self, filename, markers=True,
+                 grid=True, plotfile=None):
+        self.plot_symbols = ['o', 's', 'v', '^', '<', '>',
+                             '1', '2', '3', '4', 'p', '*',
+                             'h', 'H', '+', 'x', 'D', 'd']
+        self.linestyles = ['-', '--', '-.', ':']
+        self.filename = filename
+        self._get_cfg_file()
+        self.cfg = self._get_configuration()
+        self.data = self._get_data()
+        self.header = self._get_header()
+        self.markers = markers
+        self.grid = grid
+        self.plotfile = plotfile
+        
+    def _get_cfg_file(self):
+        fp = open(self.filename, 'r')
+        first = fp.readline()
+        match = re.match('# Beammap Timestamp: (?P<datetime_str>\w+)', first)
+        if match:
+            self.datetime_str = match.groupdict()['datetime_str']
+        second = fp.readline().strip()
+        match = re.match('# Configfile: (?P<cfgfile>\w+\.\w+)', second)
+        if match:
+            self.cfgfile = match.groupdict()['cfgfile']
+        else:
+            raise BeamPatternGeneralError('get_cfg_file', "Could not parse config file from input file")
+        three = fp.readline().strip()
+
+    def _get_configuration(self):
+        if self.cfgfile and os.path.exists(self.cfgfile):
+            config = ConfigurationPhase(self.cfgfile)
+            return config.cfg
+        else:
+            raise BeamPatternGeneralError('get_configuration', "Could not parse configuration from config file")
+
+    def _get_data(self):
+        return numpy.loadtxt(self.filename, delimiter=',')
+
+    def _get_header(self):
+        fp = open(self.filename, 'r')
+        header = ''
+        for line in fp.readlines():
+            if line[0] == '#':
+                header += line
+            else:
+                break
+        fp.close()
+        return header
+    
+    def _plot_data_amp(self, frequencies=None, linear=True,
+                       options=None):
+        title = options.title
+        print title
+        plt.ion()
+        plt.figure()
+        if frequencies is None:
+            frequencies = self.cfg['vna']['freq']
+        for i, freq in enumerate(frequencies):
+            if freq in self.cfg['vna']['freq']:
+                find = self.cfg['vna']['freq'].index(freq)*2 + 1
+                lind = i % len(self.linestyles)
+                pind = i % len(self.plot_symbols)
+                cdata = self.data[:, find] + 1j * self.data[:, find+1]
+                if linear:
+                    ydata = numpy.abs(cdata)
+                else:
+                    arg = numpy.abs(cdata)/numpy.abs(cdata).max()
+                    ind = numpy.where(arg <= 0.0)
+                    ydata = 10.0 * numpy.log10(arg)
+                    ydata[ind] = numpy.nan
+                if self.markers:
+                    plt.plot(self.data[:, 0], ydata,
+                             linestyle=self.linestyles[lind],
+                             marker=self.plot_symbols[pind],
+                             markersize=3,
+                             label='%.1f GHz' % freq)
+                else:
+                    plt.plot(self.data[:, 0], ydata,
+                             linestyle=self.linestyles[lind],
+                             label='%.1f GHz' % freq)
+        if options.xlimamp is None:
+            plt.xlim(self.cfg['azimuth']['xmin'], self.cfg['azimuth']['xmax'])
+        else:
+            plt.xlim(options.xlimamp[0], options.xlimamp[1])
+        if options.ylimamp is not None:
+            plt.ylim(options.ylimamp[0], options.ylimamp[1])
+        plt.xlabel('Azimuth (deg)')
+        if linear:
+            plt.ylabel('Beam Voltage (linear)')
+        else:
+            plt.ylabel('Beam Voltage (log)')
+        plt.legend(loc='best')
+        if title is None:
+            plt.title('%s; %s' % (self.cfg['general']['comment'], self.datetime_str))
+        else:
+            plt.title(title)
+        if self.grid:
+            plt.grid(True)
+        plt.draw()
+        plt.show()
+        if self.plotfile is not None:
+            ampfile = 'amp_' + self.plotfile
+            plt.savefig(ampfile)
+
+    def _plot_data_phase(self, frequencies=None, 
+                         options=None):
+        title = options.title
+        print title
+        plt.ion()
+        plt.figure()
+        if frequencies is None:
+            frequencies = self.cfg['vna']['freq']
+        for i, freq in enumerate(frequencies):
+            if freq in self.cfg['vna']['freq']:
+                find = self.cfg['vna']['freq'].index(freq)*2 + 1
+                lind = i % len(self.linestyles)
+                pind = i % len(self.plot_symbols)
+                cdata = self.data[:, find] + 1j * self.data[:, find+1]
+                if self.markers:
+                    plt.plot(self.data[:, 0], numpy.degrees(numpy.angle(cdata)),
+                             linestyle=self.linestyles[lind],
+                             marker=self.plot_symbols[pind],
+                             markersize=3,
+                             label='%.1f GHz' % freq)
+                else:
+                    plt.plot(self.data[:, 0], numpy.degrees(numpy.angle(cdata)),
+                             linestyle=self.linestyles[lind],
+                             label='%.1f GHz' % freq)
+        if options.xlimphase is None:
+            plt.xlim(self.cfg['azimuth']['xmin'], self.cfg['azimuth']['xmax'])
+        else:
+            plt.xlim(options.xlimphase[0], options.xlimphase[1])
+        if options.ylimphase is not None:
+            plt.ylim(options.ylimphase[0], options.ylimphase[1])
+        plt.xlabel('Azimuth (deg)')
+        plt.ylabel('Beam Phase (deg)')
+        plt.legend(loc='best')
+        if title is None:
+            plt.title('%s; %s' % (self.cfg['general']['comment'], self.datetime_str))
+        else:
+            plt.title(title)
+        if self.grid:
+            plt.grid(True)
+        plt.draw()
+        plt.show()
+        if self.plotfile is not None:
+            phasefile = 'phase_' + self.plotfile
+            plt.savefig(phasefile)
+
+    def plot_linear(self, frequencies=None, options=None):
+        self._plot_data_amp(frequencies=frequencies,
+                            linear=True, options=options)
+        self._plot_data_phase(frequencies=frequencies,
+                              options=options)
+
+    def plot_log(self, frequencies=None, options=None):
+        self._plot_data_amp(frequencies=frequencies,
+                            linear=False, options=options)
+        self._plot_data_phase(frequencies=frequencies,
+                              options=options)
+
+    
 
         
