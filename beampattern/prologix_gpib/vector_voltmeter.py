f64fb7d717a2627fa05aa6e267592ffe6bf7aadb
Gopal Narayanan <gopastro@gmail.com>
2018-06-07 11:25:49 -0400

    Add config file option for vector measurements; make small change in vv device

--- beampattern/prologix_gpib/vector_voltmeter.py
+++ beampattern/prologix_gpib/vector_voltmeter.py
@@ -10,33 +10,43 @@ class VectorVoltmeter(object):
         self.prologix.set_gpib_address(self.gpib_address)
         self.idstr = self.idstring()
 
-    def write(self, msg):
+    def write(self, msg, initgpib=True):
+        if initgpib:
+            self.prologix.set_gpib_address(self.gpib_address)
         self.prologix.write(msg)
 
-    def ask(self, msg, readlen=128):
+    def ask(self, msg, readlen=128, initgpib=True):
+        if initgpib:
+            self.prologix.set_gpib_address(self.gpib_address)        
         return self.prologix.ask(msg, readlen=readlen)
 
     def idstring(self):
         """returns ID String"""
-        self.prologix.set_gpib_address(self.gpib_address)
         ids = self.ask('*IDN?')
         return ids
     
     def meas_transmission(self, timelen, sleep=0.2):
-        self.prologix.set_gpib_address(self.gpib_address)
         self.write('SYST:FORM FP64')
-        self.write("AVER:COUN 3")
+        self.write("AVER:COUN 3", initgpib=False)
         meas = []
         t0 = time.time()
         while (time.time() - t0) < timelen:
-            str = self.ask('MEAS? TRAN')
+            str = self.ask('MEAS? TRAN', initgpib=False)
             ratio = struct.unpack('>d', str[3:11])[0]
             phase = struct.unpack('>d', str[14:])[0]
             meas.append((ratio, phase))
             time.sleep(sleep)
             #print ratio, phase
         return meas
- 
+
+    def measure_transmission_single(self, average=3):
+        self.write('SYST:FORM FP64')
+        self.write("AVER:COUN %d" % average, initgpib=False)
+        str = self.ask('MEAD? TRAN', initgpib=False)
+        ratio = struct.unpack('>d', str[3:11])[0]
+        phase = struct.unpack('>d', str[14:])[0]
+        return ratio, phase
+    
     def setup(self):
         self.prologix.set_gpib_address(self.gpib_address)        
         setup_for_analog_output = ["*RST", 
@@ -48,6 +58,6 @@ class VectorVoltmeter(object):
                                    "TRIG:SOUR BUS",
                                    "SENS TRAN"]
         for s in setup_for_analog_output:
-            self.write(s)
+            self.write(s, initgpib=False)
             time.sleep(0.010)
         
